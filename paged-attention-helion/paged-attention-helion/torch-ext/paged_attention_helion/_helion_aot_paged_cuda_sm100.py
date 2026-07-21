808793d606ab2a93df006ecad2dab5136dbdeca4
dev <dev@local>
2026-07-21 00:28:52 +0000

    AOT (default LFBO) heuristics checkpoint

--- paged-attention-helion/paged-attention-helion/torch-ext/paged_attention_helion/_helion_aot_paged_cuda_sm100.py
+++ paged-attention-helion/paged-attention-helion/torch-ext/paged_attention_helion/_helion_aot_paged_cuda_sm100.py
@@ -19,6 +19,6 @@ def key__paged_attention_v1(*args) -> int:
 def autotune__paged_attention_v1(*args) -> dict:
     """Select the optimal config for the given arguments."""
     _C = [
-        {'block_sizes': [1, 1], 'loop_orders': [[0, 1]], 'l2_groupings': [1], 'range_unroll_factors': [0, 0], 'range_warp_specializes': [None, None], 'range_num_stages': [], 'range_multi_buffers': [None, None], 'range_flattens': [None, None], 'load_eviction_policies': ['', '', '', '', ''], 'num_warps': 2, 'num_stages': 1, 'indexing': ['pointer', 'pointer', 'pointer', 'pointer', 'pointer', 'pointer'], 'atomic_indexing': [], 'pid_type': 'flat'},
+        {'block_sizes': [1, 1], 'loop_orders': [[0, 1]], 'l2_groupings': [32], 'range_unroll_factors': [0, 4], 'range_warp_specializes': [None, None], 'range_multi_buffers': [False, None], 'range_flattens': [False, False], 'load_eviction_policies': ['last', 'last', 'first', 'first', ''], 'num_warps': 1, 'num_stages': 5, 'indexing': ['pointer', 'tensor_descriptor', 'tensor_descriptor', 'tensor_descriptor', 'tensor_descriptor', 'pointer'], 'atomic_indexing': [], 'pid_type': 'persistent_interleaved', 'num_sm_multiplier': 4, 'maxnreg': 256},
     ]
     return _C[key__paged_attention_v1(*args)]
