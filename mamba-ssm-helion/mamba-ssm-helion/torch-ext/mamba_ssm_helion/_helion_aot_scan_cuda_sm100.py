feedfbff172014fbbcf209bbae8e5c67dce9bcae
dev <dev@local>
2026-07-21 00:16:51 +0000

    AOT (default LFBO) heuristics checkpoint

--- mamba-ssm-helion/mamba-ssm-helion/torch-ext/mamba_ssm_helion/_helion_aot_scan_cuda_sm100.py
+++ mamba-ssm-helion/mamba-ssm-helion/torch-ext/mamba_ssm_helion/_helion_aot_scan_cuda_sm100.py
@@ -12,13 +12,17 @@ import torch
 
 def key__selective_scan(*args) -> int:
     """Select config index for the given arguments (also serves as cache key)."""
-    # No features needed
-    return 0
+    _arg0_dim0 = int(args[0].shape[0]) if len(args) > 0 and isinstance(args[0], torch.Tensor) and args[0].ndim > 0 else 0
+    if _arg0_dim0 <= 2.0:
+        return 1
+    else:
+        return 0
 
 
 def autotune__selective_scan(*args) -> dict:
     """Select the optimal config for the given arguments."""
     _C = [
-        {'block_sizes': [1, 4], 'loop_orders': [[0, 1]], 'l2_groupings': [1], 'range_unroll_factors': [0, 0], 'range_warp_specializes': [None, None], 'range_num_stages': [0, 0], 'range_multi_buffers': [None, None], 'range_flattens': [None, None], 'load_eviction_policies': ['', '', '', '', '', '', ''], 'num_warps': 1, 'num_stages': 1, 'indexing': ['pointer', 'pointer', 'pointer', 'pointer', 'pointer', 'pointer', 'pointer', 'pointer'], 'atomic_indexing': [], 'pid_type': 'flat'},
+        {'block_sizes': [1, 16], 'loop_orders': [[1, 0]], 'l2_groupings': [32], 'range_unroll_factors': [0, 2], 'range_warp_specializes': [None, False], 'range_num_stages': [0, 4], 'range_multi_buffers': [None, True], 'range_flattens': [None, True], 'load_eviction_policies': ['last', 'first', '', '', '', '', ''], 'num_warps': 2, 'num_stages': 1, 'indexing': ['tensor_descriptor', 'tensor_descriptor', 'tensor_descriptor', 'pointer', 'pointer', 'pointer', 'pointer', 'tensor_descriptor'], 'atomic_indexing': [], 'pid_type': 'flat'},
+        {'block_sizes': [1, 2], 'loop_orders': [[1, 0]], 'l2_groupings': [8], 'range_unroll_factors': [3, 4], 'range_warp_specializes': [False, False], 'range_num_stages': [3, 3], 'range_multi_buffers': [True, None], 'range_flattens': [None, None], 'load_eviction_policies': ['last', 'first', '', '', '', '', ''], 'num_warps': 1, 'num_stages': 6, 'indexing': ['pointer', 'tensor_descriptor', 'pointer', 'tensor_descriptor', 'tensor_descriptor', 'pointer', 'pointer', 'tensor_descriptor'], 'atomic_indexing': [], 'pid_type': 'persistent_interleaved', 'num_sm_multiplier': 2, 'maxnreg': 256},
     ]
     return _C[key__selective_scan(*args)]
