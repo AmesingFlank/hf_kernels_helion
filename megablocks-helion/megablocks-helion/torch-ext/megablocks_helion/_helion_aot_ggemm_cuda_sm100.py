d1e3a9deac8f5d4cf7971c5fefd402f4a90d1e4b
dev <dev@local>
2026-07-21 01:07:54 +0000

    AOT (default LFBO) heuristics checkpoint

--- megablocks-helion/megablocks-helion/torch-ext/megablocks_helion/_helion_aot_ggemm_cuda_sm100.py
+++ megablocks-helion/megablocks-helion/torch-ext/megablocks_helion/_helion_aot_ggemm_cuda_sm100.py
@@ -19,6 +19,6 @@ def key__grouped_gemm(*args) -> int:
 def autotune__grouped_gemm(*args) -> dict:
     """Select the optimal config for the given arguments."""
     _C = [
-        {'block_sizes': [256, 256, 32], 'loop_orders': [[0, 1]], 'range_unroll_factors': [0, 0, 0, 0], 'range_warp_specializes': [None, None, None, None], 'range_num_stages': [0, 0, 0, 0], 'range_multi_buffers': [None, None, None, None], 'range_flattens': [None, None, None, None], 'load_eviction_policies': ['', '', '', ''], 'num_warps': 8, 'num_stages': 4, 'indexing': ['pointer', 'pointer', 'pointer', 'pointer', 'pointer'], 'atomic_indexing': [], 'pid_type': 'flat'},
+        {'block_sizes': [256, 256, 32], 'loop_orders': [[0, 1]], 'range_unroll_factors': [0, 0, 2, 0], 'range_warp_specializes': [None, None, False, None], 'range_num_stages': [0, 1, 2, 0], 'range_multi_buffers': [None, None, None, False], 'range_flattens': [None, None, True, False], 'load_eviction_policies': ['last', 'last', '', 'last'], 'num_warps': 8, 'num_stages': 6, 'indexing': ['pointer', 'pointer', 'tensor_descriptor', 'tensor_descriptor', 'tensor_descriptor'], 'atomic_indexing': [], 'pid_type': 'flat', 'epilogue_subtile': 2},
     ]
     return _C[key__grouped_gemm(*args)]
